bb6efa85fd209527241b7a73e124b169999ef0a6
waitingForY <1154043153@qq.com>
2016-07-06 11:45:50 +0800

    display dameon program

--- src/progress/daemon/01setsid.c
+++ src/progress/daemon/01setsid.c
@@ -4,6 +4,8 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 
 #define ERR_EXIT(m) \
@@ -21,13 +23,30 @@ int setup_daemon(void)
 	if(pid>0)
 	  exit(EXIT_SUCCESS);
 	else if(pid==0)
-	  setsid();
+	{
+		//创建一个会话期；
+		setsid();
+		//更改当前的工作目录：
+		chdir("/");
+		//重定向0,1,2三个标准io；
+		int i;
+		for(i=0;i<3;i++)
+		{
+			close(i);
+		}
+		int ret=open("/dev/null",O_RDWR);
+		if(ret==-1)
+		  ERR_EXIT("open");
+		dup(0);
+		dup(0);
+	}
 	return 0;
 }
 
 int main(void)
 {
 	setup_daemon();
+	printf("hello i am daemon\n");
 	while(1);
 	return 0;
 }
