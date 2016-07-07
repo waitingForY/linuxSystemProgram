d6bceb080c754640e2f5d76ab3970acfafa29cbc
waitingForY <1154043153@qq.com>
2016-07-07 21:57:00 +0800

    using fcntl function to add filelock and unlock

--- src/fileio/01file/10fcntl.c
+++ src/fileio/01file/10fcntl.c
@@ -14,21 +14,52 @@
 		exit(EXIT_FAILURE); \
 	}while(0)
 
+
+/*
+ *add read lock using fcntl function
+ *
+ */
+int set_lock(int fd)
+{
+	struct flock lck;
+	lck.l_type=F_RDLCK;
+	lck.l_whence=SEEK_SET;
+	lck.l_start=0;
+	lck.l_len=0;
+	int ret=fcntl(fd,F_SETLK,&lck);
+	return ret;
+}
+
+
+/*
+ *clear lock using file discripation
+ */
+
+
+int clr_lock(int fd)
+{
+	struct flock lck;
+	lck.l_type=F_UNLCK;
+	int ret=fcntl(fd,F_SETLK,&lck);
+	return ret;
+}
+
 int main(void)
 {
 	int fd;
 	fd=open("test.txt",O_RDWR);
 	if(fd==-1)
 	  ERR_EXIT("open error");
-	//close stdout;
-	/*
-	close(1);
-	dup(fd);
-	*/
-	//dup2(fd,1);
-	
-	close(1);
-	fcntl(fd,F_DUPFD,0);
-	printf("hello\n");
+	int ret;
+	ret=set_lock(fd);
+	if(ret==-1)
+	  ERR_EXIT("set_lock error");
+	printf("set_lock success\n");
+	printf("press any key to unlock\n");
+	getchar();
+	ret=clr_lock(fd);
+	if(ret==-1)
+	  ERR_EXIT("clear lock error");
+	printf("clear lock success\n");
 	return 0;
 }
